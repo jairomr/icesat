b95bd6406da6d561b54189a92d671d269a7c6f80
Jairo Matos da Rochal <devjairomr@gmail.com>
2023-08-23 23:49:58 -0300

    filter 5

--- Icesat2/icesat2/function.py
+++ Icesat2/icesat2/function.py
@@ -1,3 +1,4 @@
+import pickle
 import geohash
 from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
@@ -8,19 +9,22 @@ from icesat2.config import logger, settings
 def to_geohash(row):
     try:
         return geohash.encode(
-            latitude=row['latitude'], longitude=row['longitude'], precision=3
+            latitude=row['latitude'], longitude=row['longitude'], precision=5
         )
     except KeyError:
         return geohash.encode(
-            latitude=row['lat_ph'], longitude=row['lon_ph'], precision=3
+            latitude=row['lat_ph'], longitude=row['lon_ph'], precision=5
         )
     
 
 
 def geohash_lapig(tmp_df):
+    with open('geohash_5_maior_20.obj') as f:
+        filter_geohash = pickle.load(f)
+    # '6v7','6vk','6v5','6vh'
     tmp_df['geohash'] = tmp_df.apply(to_geohash, axis=1)
     filtered_df = tmp_df[
-        tmp_df['geohash'].str.startswith(('6v7','6vk','6v5','6vh'))
+        tmp_df['geohash'].str.startswith(filter_geohash)
     ].copy()
     return filtered_df
 
